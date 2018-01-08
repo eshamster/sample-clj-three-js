85cb4601cdeb83f0a130a4bf892618eb3fba37f7
eshamster <hamgoostar@gmail.com>
2018-01-08 21:43:59 +0000

    Try "setup" and "teardown" idiom for hot reloading

--- src/sample_clj_three_js_figwheel/core.cljs
+++ src/sample_clj_three_js_figwheel/core.cljs
@@ -9,9 +9,18 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
+(println @app-state)
+
+(defn setup []
+  (println "do setup"))
+
+(defn teardown []
+  (println "do teardown"))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
+  (teardown)
+  (setup)
 )

0d21b8f9eeea496b7aa2065e8a276920709885db
eshamster <hamgoostar@gmail.com>
2018-01-09 22:29:09 +0000

    Update to try more hot updating

--- src/sample_clj_three_js_figwheel/core.cljs
+++ src/sample_clj_three_js_figwheel/core.cljs
@@ -6,10 +6,28 @@
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:text "Hello world!"}))
+(defonce frame-counter (atom 0))
+
+(defn change-color [mesh new-color]
+  (aset mesh "material" "color" (js/THREE.Color. new-color))
+  (aset mesh "material" "needsUpdate" true))
 
 (defn update-mesh [mesh]
   (aset mesh "rotation" "x" (+ 0.01 (.-x (.-rotation mesh))))
-  (aset mesh "rotation" "y" (+ 0.02 (.-y (.-rotation mesh)))))
+  (aset mesh "rotation" "y" (+ 0.02 (.-y (.-rotation mesh))))
+  (when (= (mod @frame-counter 120) 0)
+    (let [mod-counter (mod @frame-counter 360)]
+      (cond (= mod-counter   0) (change-color mesh 0xff0000)
+            (= mod-counter 120) (change-color mesh 0x00ff00)
+            (= mod-counter 240) (change-color mesh 0x0000ff)))))
+
+(defn update-camera [camera]
+  (let [z (.-z (.-position camera))]
+    (when (< z 1000)
+      (aset camera "position" "z" (+ 0.1 z)))))
+
+(defn update-others []
+  (swap! frame-counter #(+ % 1)))
 
 (defn init []
   (let [scene (js/THREE.Scene.)
@@ -35,7 +53,9 @@
     (.appendChild js/document.body (.-domElement renderer))
     ;; Kick off the animation loop updating
     (defn render []
+      (update-others)
       (update-mesh mesh)
+      (update-camera p-camera)
       (.render renderer scene p-camera))
 
     (defn animate []
